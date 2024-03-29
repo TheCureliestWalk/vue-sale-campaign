19689effc564e60e99e11db64bd9b113b30034aa
Iho Somnam <ihosomnam4741@gmail.com>
2024-03-29 16:01:56 +0700

    :wrench: On Top Calc

--- src/App.vue
+++ src/App.vue
@@ -65,28 +65,95 @@
               <span>Percentage Discount</span></label
             >
           </div>
-          <input type="number" v-model="couponDiscount" />
+          <input type="number" v-model="couponDiscountAmount" />
+          <code
+            >Discounted: <b>{{ couponDiscount }} THB</b></code
+          >
         </div>
 
         <!-- 2. On Top -->
         <div>
           <h4>2. On Top</h4>
-          <p>15% off on Clothing || Or points (1 pts = 1 THB, max 20%)</p>
-          <p>TODO</p>
+          <!-- On Top Type Selection -->
+          <div class="flex items-center pl">
+            <label for="onTopByCategoryPercentage">
+              <input
+                type="radio"
+                id="onTopByCategoryPercentage"
+                name="onTopByCategoryPercentage"
+                value="categoryPercentage"
+                v-model="onTopDiscountType"
+              />
+              <span>By Category Percentage</span>
+            </label>
+            <label for="onTopByPoint">
+              <input
+                type="radio"
+                id="onTopByPoint"
+                name="onTopByPoint"
+                value="point"
+                v-model="onTopDiscountType"
+              />
+              <span>By Point</span>
+            </label>
+          </div>
+
+          <!-- On Top Category Selection -->
+          <div v-if="onTopDiscountType === 'categoryPercentage'">
+            <label for="onTopCategorySelection">Category: </label>
+            <select id="categorySelection" name="categorySelection" v-model="onTopDiscountCategory">
+              <option value="clothing">Clothing</option>
+              <option value="electronics">Electronics</option>
+              <option value="accessories">Accessories</option>
+            </select>
+          </div>
+          <div>
+            <label for="onTopDiscountAmount">{{
+              onTopDiscountType === 'categoryPercentage'
+                ? 'Percentage: '
+                : 'Point: (max 20% of Total Price)'
+            }}</label>
+            <input
+              id="onTopDiscountAmount"
+              name="onTopDiscountAmount"
+              type="number"
+              v-model="onTopDiscountAmount"
+            />
+          </div>
+          <code v-if="onTopDiscountType === 'categoryPercentage'"
+            >onTopDiscountCategory: {{ onTopDiscountCategory }}</code
+          >
+          <code>onTopDiscount: {{ onTopDiscount }}</code>
         </div>
 
         <!-- 3. Special Campaign (Seasonal) -->
         <div>
           <h4>3. Special Campaign (Seasonal)</h4>
-          <small>Example Discount: 40 THB at every 300 THB</small>
-          <p>
-            Every <input type="number" v-model="seasonalDiscountEveryPrice" /> THB, get
-            <input type="number" v-model="seasonalDiscountPrice" /> THB discount
-          </p>
+          <small
+            >Example Discount: {{ seasonalDiscountPrice }} THB at every
+            {{ seasonalDiscountEveryPrice }} THB</small
+          >
+          <div>
+            <!-- I don't know how to call this speical deal -->
+            <label for="seasonalDiscountEveryPrice">Every Total Price (THB)</label>
+            <input
+              type="number"
+              id="seasonalDiscountEveryPrice"
+              name="seasonalDiscountEveryPrice"
+              v-model="seasonalDiscountEveryPrice"
+            />
+            <label for="seasonalDiscountEveryPrice">Will Get (THB) off</label>
+            <input
+              type="number"
+              id="seasonalDiscountPrice"
+              name="seasonalDiscountPrice"
+              v-model="seasonalDiscountPrice"
+            />
+          </div>
 
-          <p>
-            Seasonal discounted <b>{{ seasonalDiscount }} THB</b>
-          </p>
+          <code>
+            Seasonal discounted: <b>{{ seasonalDiscount }} THB</b>
+          </code>
         </div>
       </div>
 
@@ -105,51 +172,85 @@
 </template>
 
 <script setup>
-import { ref, computed, watch } from 'vue'
+import { ref, computed, watchEffect } from 'vue'
 import { items } from '@/data/items.json'
 
 const selectedItems = ref([])
-const couponDiscountType = ref('fixed')
-const totalPrice = ref(0)
 
 const couponDiscount = ref(0)
+// Form handling variables
+const couponDiscountType = ref('fixed')
+const couponDiscountAmount = ref(0)
+
 const onTopDiscount = ref(0)
+const onTopDiscountType = ref('categoryPercentage') // 'point' or 'categoryPercentage'
+const onTopDiscountCategory = ref('clothing') // 'clothing' or 'electronics' or 'accessories'
+const onTopDiscountAmount = ref(0)
 
 const seasonalDiscount = ref(0)
+// Form handling variables
 const seasonalDiscountEveryPrice = ref(300)
 const seasonalDiscountPrice = ref(40)
+
+const totalPrice = ref(0)
 
 // Calculation of discount price
 
 // First Priority
-const calculateCouponDiscount = (type, couponDiscount) => {
+const calculateCouponDiscount = (type, discount) => {
   if (type == 'percentage') {
-    return (finalPrice.value = totalPrice.value - totalPrice.value * (couponDiscount / 100))
-  } else {
-    return (finalPrice.value = totalPrice.value - couponDiscount)
+    return (couponDiscount.value = totalPrice.value * (discount / 100))
   }
+  return (couponDiscount.value = discount)
+}
+
+// Second Priority
+const calculateOnTopDiscount = (type, discount) => {
+  // Category Percentage Discount
+  if (type === 'categoryPercentage') {
+    // Filter only selected category items (make it lowercase for comparison)
+    const _categoryItems = selectedItems.value.filter(
+      (item) => item.category.toLowerCase() === onTopDiscountCategory.value
+    )
+    console.log('Category Items:', _categoryItems)
+
+    // Accumulate total price of selected category items
+    const _categoryTotalPrice = _categoryItems.reduce((acc, item) => acc + item.price, 0)
+    return (onTopDiscount.value = _categoryTotalPrice * (discount / 100))
+  }
+  // ELSE: Point Discount (max 20% of Total Price)
+  return (onTopDiscount.value =
+    discount > totalPrice.value * 0.2 ? totalPrice.value * 0.2 : discount)
 }
 
 // Third Priority
 const calculateSeasonalDiscount = (seasonalDiscountPrice, seasonalDiscountEveryPrice) => {
+  // Check to prevent the divider is 0, ortherwise will get NaN
+  if (
+    typeof seasonalDiscountEveryPrice !== 'number' ||
+    typeof seasonalDiscountEveryPrice !== 'number'
+  ) {
+    return (seasonalDiscountEveryPrice.value = 0)
+  }
   const discount = computed(
-    () => Math.floor(finalPrice.value / seasonalDiscountEveryPrice) * seasonalDiscountPrice
+    () => Math.floor(totalPrice.value / seasonalDiscountEveryPrice) * seasonalDiscountPrice
   ) // Discount 40 THB at every 300 THB
   return (seasonalDiscount.value = discount.value)
 }
 
 // Final Price Calc.
 const finalPrice = computed(() => {
-  return (
-    selectedItems.value.reduce((acc, item) => acc + item.price, 0) -
-    couponDiscount.value -
-    onTopDiscount.value -
-    seasonalDiscount.value
-  )
+  const _finalPrice =
+    totalPrice.value - couponDiscount.value - onTopDiscount.value - seasonalDiscount.value
+  // Check if value is negative number and return 0 THB
+  if (_finalPrice <= 0) return 0
+  return _finalPrice
 })
 
-watch(() => {
-  calculateCouponDiscount(couponDiscountType.value, couponDiscount.value)
+// Watch and update every changes
+watchEffect(() => {
+  calculateCouponDiscount(couponDiscountType.value, couponDiscountAmount.value)
+  calculateOnTopDiscount(onTopDiscountType.value, onTopDiscountAmount.value)
   calculateSeasonalDiscount(seasonalDiscountPrice.value, seasonalDiscountEveryPrice.value)
 })
 
@@ -163,6 +264,7 @@ const addItem = (item) => {
 <style>
 .flex {
   display: flex;
+  gap: 1rem;
 }
 
 .items-center {
@@ -171,5 +273,9 @@ const addItem = (item) => {
 
 .pl {
   padding-left: 1rem;
+}
+
+label > input {
+  margin-left: 0.5rem;
 }
 </style>
